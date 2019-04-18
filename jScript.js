824f7aa44b97c0e7470649f4a445ebf487809cb3
Ruslan <alakaev0@gmail.com>
2019-04-18 22:21:09 +0500

    Первое задание

--- jScript.js
+++ jScript.js
@@ -1,5 +1,23 @@
 'use strict';
 
-var secondsun = 1;
-console.log(secondsun);
-alert(12);
+// var number=5;
+// var string = "word";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+let money = +prompt("Ваш бюджет в месяц?", ''),
+time = prompt("Введите дату в формате YYYY-MM-DD", ''),
+appData = {
+    budget: money,
+    timeData:{},
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+alert('Ваш бюджет на один день составляет: ' + (appData.budget / 30).toFixed(2));
+
