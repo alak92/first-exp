51cfc6b5673d35d890312e81a9285436c612fd05
Ruslan Alakaev <49761756+alak92@users.noreply.github.com>
2019-04-19 20:56:10 +0500

    Add files via upload

--- jScript.js
+++ jScript.js
@@ -19,5 +19,12 @@ appData = {
     savings: false
 };
 
-alert('Ваш бюджет на один день составляет: ' + (appData.budget / 30).toFixed(2));
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
